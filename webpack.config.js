644847cb2727a587606a58663461afcad4e0e923
Justin Vrana <justin.vrana@gmail.com>
2021-09-06 18:27:25 -0700

    src

---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, args) => {
+    const isProduction = args && args['mode'] === 'production';
+    console.log('');
+    console.log(isProduction ? 'PRODUCTION BUILD' : 'DEVELOPMENT BUILD');
+    console.log('');
+
+    const config = {
+        entry: {
+            'scripts/main': path.resolve('./src/index.tsx'),
+        },
+        output: {
+            path: path.resolve('./dist')
+        },
+        target: 'web',
+        devtool: isProduction ? false : 'source-map',
+        optimization: {
+            splitChunks: {
+                // always create vendor.js
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'scripts/vendor',
+                        chunks: 'initial',
+                        enforce: true,
+                    },
+                },
+            },
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.html', '.txt']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    // eslint
+                    enforce: 'pre',
+                    use: [
+                        {
+                            options: {
+                                eslintPath: require.resolve('eslint'),
+                            },
+                            loader: require.resolve('eslint-loader'),
+                        }
+                    ]
+                },
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true
+                        }
+                    }]
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates 'style' nodes from JS strings
+                        "style-loader",
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader"
+                    ]
+                }
+            ]
+        },
+        watchOptions: {
+            aggregateTimeout: 100,
+            ignored: /node_modules/,
+            poll: 300
+        },
+        devServer: {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            static: {
+                directory: path.join(__dirname, 'src/static'),
+                publicPath: '/'
+            },
+            compress: false,
+            port: 3000,
+            historyApiFallback: true,
+            hot: true,
+            client: {
+                logging: 'error'
+            }
+        },
+
+        plugins: [
+            new webpack.EnvironmentPlugin({
+                NODE_ENV: isProduction ? 'production' : 'development',
+                DEBUG: !isProduction
+            }),
+
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: '**/*',
+                        to: path.resolve('./dist/'),
+                        context: './src/static'
+                    }
+                ]
+            })
+        ]
+    };
+
+    if (isProduction) {
+        config.optimization.minimize = true;
+        config.optimization.minimizer = [
+            new TerserPlugin({extractComments: false}),
+            new OptimizeCSSAssetsPlugin({}),
+        ]
+    }
+
+    return config;
+}
